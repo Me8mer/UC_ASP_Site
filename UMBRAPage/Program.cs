8e4b5aba22dae075908e17e5140f452f82947284
Peter <p.vidlicka@gmail.com>
2025-04-09 20:53:45 +0200

    Fix db issue

--- UMBRAPage/Program.cs
+++ UMBRAPage/Program.cs
@@ -20,6 +20,11 @@ builder.Services.AddSession(options =>
 
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
